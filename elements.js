385b9659175945c1d92e11a0607d8ebdae9f46c0
Ido <ido2267@gmail.com>
2018-07-21 21:49:11 +0300

    first commit

---
+++ elements.js
@@ -0,0 +1,11 @@
+function init (){
+  //  alert (The_h2.innerHTML);
+}
+function showInput(){
+  //  alert(name_txt.value);
+//  The_h2.innerHTML = name_txt.value;
+var num1 = num1_txt.value;
+var num2 = num2_txt.value;
+var num3 = Number(num1) + Number(num2);
+The_h2.innerHTML = num3;
+}
